fdede223137e7b9956b2cc64ca9d3db84739f85e
saleh8787 <salehsubhani48@gmail.com>
2024-10-15 21:59:05 +0500

    do sorting utils

--- SortingUtils.cpp
+++ SortingUtils.cpp
@@ -4,7 +4,8 @@ void SortingUtils::bubbleSort(std::vector<int>& arr)
  {
     int n = arr.size();
     for (int i = 0; i < n - 1; ++i) {
-        for (int j = 0; j < n - i - 1; ++j) {
+        for (int j = 0; j < n - i - 1; ++j)
+         {
             if (arr[j] > arr[j + 1]) {
                 std::swap(arr[j], arr[j + 1]);
             }
